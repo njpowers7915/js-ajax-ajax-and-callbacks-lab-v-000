a75d2e40ef6d4b98c7d77fed700221d3c4222b84
Nicholas Powers <njpowers7915@gmail.com>
2018-08-10 12:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function searchRepositories(searchTerms) {
 $.get(searchTerms, searchRepositories()).fail(displayError());
 
 function displayError (error) {
-  return error
+  return "I'm sorry, there's been an error. Please try again."
 }
