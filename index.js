58be71edfccc53c8dc072f1a0e394e3a9db3c2d8
Nicholas Powers <njpowers7915@gmail.com>
2018-08-10 12:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function searchRepositories(searchTerms) {
 $.get(searchTerms, searchRepositories()).fail(function(error) {
   console.log('Something went wrong: ' + error);
 });
+
+function displayError (error) {
+  return error
+}
