a12e1fe540c0231c00ca108923f261dd7dd4fddf
Nicholas Powers <njpowers7915@gmail.com>
2018-08-10 13:15:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function searchRepositories(searchTerms) {
   })
 }
 
-function show() {
+function showCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
